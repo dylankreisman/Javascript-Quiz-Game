714f7938aabc9f06083dd5396fcbf396bb680a11
Dr Dyl <dylkreisman@gmail.com>
2022-06-28 23:55:46 -0500

    final js commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//characters
 var startBtn = document.querySelector("#start-btn");
 console.log(startBtn)
 var quesContainer = document.querySelector(".container");
@@ -8,14 +9,54 @@ var intro = document.querySelector('.card')
 var highScores = document.querySelector("#score");
 var highScoresDiv = document.querySelector('#highscores')
 highScoresDiv.style = "display: none;"
+var woodstock = "What year was the first Woodstock Festival?"
+var timer = document.querySelector("#time");
+var timeLeft = 60;
+var questions 
 
+//start click
 startBtn.addEventListener("click", function() {
-    var setTime = setInterval(function(){
-        timer.textContent = timeLeft
-        timeLeft--
-    }, 1000)
+   var setTime = setInterval(function(){
+       timer.textContent = timeLeft
+       timeLeft--
+   }, 1000)
+  
+
+   intro.style = "display: none;"
    
-    var timer = document.querySelector("#time");
-    var timeLeft = 60;
-    intro.style = "display: none;"
-    questText.innerHTML = zepQues
+   //beginning first question
+   questText.innerHTML = zepQues
+   var zeppelin = ['Robert Plant ', 'Jimmy Page ', 'John Bonham ', 'Bono'];
+   var zepNodes = zeppelin.map(lang => {
+      var li = document.createElement('li');
+       ansOne.appendChild(li)
+      li.textContent = lang;
+      li.addEventListener("click", function(e){
+       console.log(e.target.textContent)
+       if(e.target.textContent !== 'Jimmy Page '){
+           timeLeft-=10
+       }
+
+       clearInterval(setTime)
+// prompt and score
+       var userName = prompt("Enter your name to match with your score!")
+       localStorage.setItem(userName, timeLeft)
+       highScoresDiv.style = "display: block; font-size: 20px;"
+       for (let i = 0; i < localStorage.length; i++) {
+       var li = document.createElement('li');
+       highScores.appendChild(li)
+       li.textContent = localStorage.key(i) + ': ' + localStorage.getItem(localStorage.key(i))
+       }
+      })
+     
+
+  
+   });
+  
+  
+  
+
+
+
+
+});
