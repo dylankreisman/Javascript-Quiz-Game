398bb4eb091585a9bb62077af1269702535ad10b
Dr Dyl <dylkreisman@gmail.com>
2022-06-28 21:04:42 -0500

    java variables

---
+++ script.js
@@ -0,0 +1,10 @@
+var startBtn = document.querySelector("#start-btn");
+console.log(startBtn)
+var quesContainer = document.querySelector(".container");
+var zepQues = "Who is the lead guitarist for Led Zeppelin?";
+var questText = document.querySelector(".question");
+var ansOne = document.querySelector('#answers');
+var intro = document.querySelector('.card')
+var highScores = document.querySelector("#score");
+var highScoresDiv = document.querySelector('#highscores')
+highScoresDiv.style = "display: none;"
